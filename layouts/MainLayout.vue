884eee4e9600e0c73f316e80afb51021b814b4d3
Asad Anwar <asadanwar60@gmail.com>
2024-02-14 22:46:22 +0530

    first commit

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="MainLayout" class="w-full fixed z-50">
+    <div id="Top" class="w-full bg-[#FAFAFA] border-b md:block hidden">
+      <ul
+        class="flex items-center justify-end text-xs text-[#333333] font-light px-2 h-10 bg-[#FAFAFA] max-w-[1200px ̰]"
+      ></ul>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
